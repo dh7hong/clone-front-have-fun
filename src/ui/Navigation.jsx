5daa5c97d8729fbe48892d6fe8a99ea831df88f9
dh7hong <dh7hong@gmail.com>
2023-11-27 18:37:10 +0900

    reloads on handleLogout

--- src/ui/Navigation.jsx
+++ src/ui/Navigation.jsx
@@ -2,23 +2,31 @@ import React from "react";
 import * as S from "../shared/style/NavigationStyle";
 import { useLocation, useNavigate } from "react-router-dom";
 
-const memberId = localStorage.getItem("memberId");
-const NAVIGATION_BAR = [
-  
-  { name: "Home", navigate: `/home` },
-  { name: "Profile", navigate: `/api/users/${memberId}/profile` },
-  { name: "Diary", navigate: `/api/users/${memberId}/diary` },
-  { name: "Juke Box", navigate: `/api/users/${memberId}/jukebox` },
-  { name: "Posts", navigate: `/api/users/${memberId}/posts` },
-  { name: "Add Friend", navigate: `/api/users/${memberId}/friends`},
-  { name: "Guest Book", navigate: `/api/users/${memberId}/guestbook` },
-];
+
+
 
 export default function Navigation() {
   const navigate = useNavigate();
+
+  const memberId = localStorage.getItem("memberId");
   
+  const NAVIGATION_BAR = [
+    
+    { name: "Home", navigate: `/home` },
+    { name: "Profile", navigate: `/api/users/${memberId}/profile` },
+    { name: "Diary", navigate: `/api/users/${memberId}/diary` },
+    { name: "Juke Box", navigate: `/api/users/${memberId}/jukebox` },
+    { name: "Posts", navigate: `/api/users/${memberId}/posts` },
+    { name: "Add Friend", navigate: `/api/users/${memberId}/friends`},
+    { name: "Guest Book", navigate: `/api/users/${memberId}/guestbook` },
+  ];
 
   const onClickBar = (url) => () => {
+
+    
+
+
+
     navigate(url);
   };
   return (
