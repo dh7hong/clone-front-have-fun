4a5b2393b856b272f8b30e275e52cf8a6f0ae964
dh7hong <dh7hong@gmail.com>
2023-12-03 03:58:26 +0900

    "deleted!"

--- src/pages/Guestbook.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Guestbook() {
-  return <div>Guestbook</div>;
-}
--- src/pages/Jukebox.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Jukebox() {
-  return <div>Juke Box</div>;
-}
